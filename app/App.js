491c9a478eaad39fed35ffcd4401cd0222882848
Josh <jdlazar2011@gmail.com>
2018-12-11 12:39:30 -0500

    updates to router, static props in statefulComponent

--- app/App.js
+++ app/App.js
@@ -1,28 +1,18 @@
-/*
+
 import React from 'react'
-import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import { Switch, Route } from 'react-router-dom'
 import StatefulLayout from '../src/component/StatefulLayout'
 
 const App = () => {
-
   return (
-    <Router history={browserHistory}>
-      <Route path='/' component={StatefulLayout}>
-        <IndexRoute component={StatefulLayout} />
-        <Route path='why' component={StatefulLayout} />
-        <Route path='viewBracelet' component={StatefulLayout} />
-      </Route>
-    </Router>
+    <div className=''>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={StatefulLayout} />
+        <Route path='/members' component={SteatefulLayout} />
+      </Switch>
+    </div>
   )
-}
-
-export default App
-*/
-import React from 'react'
-import Layout from '../src/component/StatefulLayout'
-
-const App = () => {
-  return <Layout />
 }
 
 export default App
--- src/component/StatefulLayout.js
+++ src/component/StatefulLayout.js
@@ -1,19 +1,26 @@
 import React, { Component } from 'react'
-
+import PropTypes from 'prop-types'
 export default class StatefulLayout extends Component {
-  constructor () {
-    super()
-    this.state = {}
-  }
+  static PropTypes = {}
+  static DefaultProps = {}
+  state = {}
 
   render () {
     return (
-      <div>
-        Hello from Main!
+      <div className=''>
+        <div>
+          {arr.map(el => {
+            return (
+              <ChildComponent
+                key={el.something}
+                user={user} />
+            )
+          })}
+        </div>
+        <span>
+          Hello from Main!
+        </span>
       </div>
     )
   }
 }
-
-// {this.props.children}
-//  PropTypes
