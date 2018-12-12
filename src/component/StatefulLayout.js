5fabf9b06dccbb8841039935eb671b554fd42db4
Josh <jdlazar2011@gmail.com>
2018-12-12 13:04:41 -0500

    updated base css file template

--- src/component/StatefulLayout.js
+++ src/component/StatefulLayout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+
 export default class StatefulLayout extends Component {
   static PropTypes = {}
   static DefaultProps = {}
